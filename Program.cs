964ada78c20b395c0969ec6a5743ebc590298738
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-06 16:10:04 +0530

    Initiating the game

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Game.Exceptions;
+using Game.Validation;
+using Game.GameFlow;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        WordGuessGame game = new WordGuessGame();
+        game.Start();
+    }
+}
