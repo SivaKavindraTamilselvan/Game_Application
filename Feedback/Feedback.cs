a5be6b88df795e0b44e0520a582efcff442b44cb
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-06 16:06:14 +0530

    Validating the input is inserted

---
+++ Feedback/Feedback.cs
@@ -0,0 +1,47 @@
+namespace Game.Feedback;
+
+public class FeedbackGenerator
+{
+    public string GetFeedback(string guessed_word,string actual_word)
+    {
+        char[] result = new char[5];
+        char[] hidden_word = actual_word.ToCharArray();
+        for(int i = 0; i < 5; i++)
+        {
+            if(guessed_word[i] == hidden_word[i])
+            {
+                result[i]='G';
+                hidden_word[i] = '*';
+            }
+        }
+        for(int i = 0; i < 5; i++)
+        {
+            if(result[i]=='G')
+            {
+                continue;
+            }
+            bool check = false;
+            for(int j = 0; j < 5; j++)
+            {
+                if(hidden_word[j]!='*' && guessed_word[i] == hidden_word[j])
+                {
+                    hidden_word[j] = '*';
+                    check = true;
+                    break;
+                }
+            }
+            if(check)
+            {
+                result[i]='Y';
+            }
+            else
+            {
+                result[i]='X';
+            }
+        }
+        string final_result = new string(result);
+        return final_result;
+    }
+
+    
+}
