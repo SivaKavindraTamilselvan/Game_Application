f4201cf476923441e84f77629c22f0e1f86cdac2
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-06 16:09:55 +0530

    Added print for the feedback

--- Feedback/Feedback.cs
+++ Feedback/Feedback.cs
@@ -2,46 +2,76 @@ namespace Game.Feedback;
 
 public class FeedbackGenerator
 {
-    public string GetFeedback(string guessed_word,string actual_word)
+    public string GetFeedback(string guessed_word, string actual_word)
     {
         char[] result = new char[5];
         char[] hidden_word = actual_word.ToCharArray();
-        for(int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++)
         {
-            if(guessed_word[i] == hidden_word[i])
+            if (guessed_word[i] == hidden_word[i])
             {
-                result[i]='G';
+                result[i] = 'G';
                 hidden_word[i] = '*';
             }
         }
-        for(int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++)
         {
-            if(result[i]=='G')
+            if (result[i] == 'G')
             {
                 continue;
             }
             bool check = false;
-            for(int j = 0; j < 5; j++)
+            for (int j = 0; j < 5; j++)
             {
-                if(hidden_word[j]!='*' && guessed_word[i] == hidden_word[j])
+                if (hidden_word[j] != '*' && guessed_word[i] == hidden_word[j])
                 {
                     hidden_word[j] = '*';
                     check = true;
                     break;
                 }
             }
-            if(check)
+            if (check)
             {
-                result[i]='Y';
+                result[i] = 'Y';
             }
             else
             {
-                result[i]='X';
+                result[i] = 'X';
             }
         }
         string final_result = new string(result);
         return final_result;
     }
 
-    
+    public void PrintColoredFeedback(string guess, string feedback)
+    {
+        for (int i = 0; i < guess.Length; i++)
+        {
+            if (feedback[i] == 'G')
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (feedback[i] == 'Y')
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            Console.Write(guess[i] + "  ");
+        }
+        Console.ResetColor();
+
+        Console.WriteLine();
+
+        foreach (char c in feedback)
+        {
+            Console.Write(c + "  ");
+        }
+
+        Console.WriteLine();
+        Console.ResetColor();
+    }
 }
