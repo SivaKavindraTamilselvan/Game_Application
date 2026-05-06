1336a8fbcca456adb6762a3acbc9f2c7c37c2bbb
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-06 16:03:42 +0530

    Basic Validaition Functions are entered

---
+++ Validation/Validation.cs
@@ -0,0 +1,27 @@
+using Game.Exceptions;
+
+namespace Game.Validation;
+
+public class WordGuessValidation
+{
+    public void ValidateFunction(string guessed_word,List<string> guessed_words)
+    {
+        if(guessed_word.Trim() == "")
+        {
+            throw new InvalidGuessException("Input cannot be empty");
+        }
+        if(guessed_word.Length<5)
+        {
+            throw new InvalidGuessException("Input Length cannot be less than 5. Need to enter exactly 5 letter word");
+        }
+        if(guessed_word.Length>5)
+        {
+            throw new InvalidGuessException("Input Length cannot be greater than 5. Need to enter exactly 5 letter word");
+        }
+        if(!guessed_word.All(char.IsLetter))
+        {
+            throw new InvalidGuessException("Input should be only characters not any other symbols or numbers");
+        }
+        
+    }
+}
